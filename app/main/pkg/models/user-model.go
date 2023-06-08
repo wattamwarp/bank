dc0332ade5aa60ed468223a15c3764448c4b707a
Padmanabh Wattamwar <wattamwarp874@gmail.com>
2023-06-08 11:27:07 +0530

    folder restructuring

--- app/main/pkg/models/user-model.go
+++ app/main/pkg/models/user-model.go
@@ -39,17 +39,22 @@ import (
 // 	IsDeleted     bool      `json:"isDeleted"`
 // }
 
-
-
 type UserDetails struct {
 	gorm.Model
 	Name          string    `json:"name"`
-	// Other fields of UserDetails struct
+	Address       string    `json:"address"`
+	PinCode       int       `json:"pincode"`
+	PhoneNumber   int       `json:"phonenumber"`
+	EmailID       string    `json:"emailId"`
+	DOB           string    `json:"dob"`
+	IncomeRange   int       `json:"incomeRange"`
+	BranchName    string    `json:"branchName"`
+	BranchCode    int       `json:"branchCode"`
+	BranchAddress string    `json:"branchAddress"`
 	EmployeeID    int       `json:"employee_id"` // Foreign key
 	Employee      Employees `json:"employees" gorm:"foreignKey:EmployeeID;constraint:OnUpdate:CASCADE,OnDelete:RESTRICT;belongsTo:EmployeeID"`
 	IsDeleted     bool      `json:"isDeleted"`
 }
-
 
 var db *gorm.DB
 
